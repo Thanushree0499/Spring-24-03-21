6ef97481b6c780102a8b10c65c54c16b04333171
Thanushree0499 <73820030+Thanushree0499@users.noreply.github.com>
2021-03-26 16:28:29 +0530

    Add files via upload

---
+++ chimney-app/src/main/java/com/xworkz/chimney/Chimney.java
@@ -0,0 +1,24 @@
+package com.xworkz.chimney;
+
+public class Chimney {
+
+	private String brand;
+	
+	private double price;
+
+	public Chimney(String brand, double price) {
+		super();
+		this.brand = brand;
+		this.price = price;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+	
+	
+}
